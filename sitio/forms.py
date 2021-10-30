3e3a4721e9a392680b3142479a9ad5389d66dd39
Francisco Silva <franciscsilva11 @gmail.com>
2021-10-30 14:15:22 -0300

    first commit

---
+++ sitio/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from .models import Articulo
+
+class FormArticulo(forms.ModelForm):
+
+    #campos del modelo
+    class Meta:
+        model = Articulo
+        fields = ('seccion', 'fecha_publicacion', 'titulo', 'contenido', 'imagen')
+        widgets = {
+            'fecha_publicacion': forms.SelectDateWidget(attrs={'class': 'pub_fecha_publicacion'}),
+            'titulo': forms.TextInput(attrs={'class': 'pub_titulo'}),
+            'contenido': forms.Textarea(attrs={'class': 'pub_contenido'}),
+            'imagen': forms.FileInput(attrs={'name':'imagen_adjunta', 'class': 'pub_imagen'}),
+        }
